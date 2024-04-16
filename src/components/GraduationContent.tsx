7b2916d038029182a9363b89fb65b7d79661fbb0
Miguel Oliveira <miguelmigpt@gmail.com>
2024-04-16 16:36:56 +0100

    Prettify and keep git up to date

--- src/components/GraduationContent.tsx
+++ src/components/GraduationContent.tsx
@@ -1,38 +1,53 @@
 'use client'
 import { getLens } from "@/network/getLens"
-import { Button } from "@mui/material"
-import { useState } from "react"
-import ModelCard from "./ModelCard"
+import { TextareaHTMLAttributes, useState } from "react"
+import ModelCard, { ModelCardProps } from "./ModelCard"
+import _ from 'lodash';
+
+const vars = ["Cilindro", "Esfera"]
 
 function GraduationContent() {
   
-  const [data, setData] = useState([])
+  const [data, setData] = useState<ModelCardProps[]>([])
 
   console.log(`data`, data)
 
+  const debouncedOnChange = _.debounce(() => {
+    let cilElem = document.getElementById(vars[0]) as unknown as TextareaHTMLAttributes<HTMLTextAreaElement>;
+    let esfElem = document.getElementById(vars[1]) as unknown as TextareaHTMLAttributes<HTMLTextAreaElement>;
+    if (cilElem && esfElem) {
+      cilElem.value = cilElem.value?.toString().replace(/[^0-9-.]/g, '')
+      esfElem.value = esfElem.value?.toString().replace(/[^0-9-.]/g, '')
+      getLens({cil: cilElem.value ?? '0.0', esf: esfElem.value ?? '0.0' }).then((data) => {
+        console.log(`data`, data)
+        setData(data)
+      })
+    }
+    
+  }, 1000)
+
+  const handleChange = (event : any) => {
+    debouncedOnChange();
+  };
+
   return (
     <div className="flex-1 p-2 overflow-hidden">
-      <div className="grid grid-cols-2 gap-4 items-center justify-center">
-        {["Cilindro", "Esfera"].map((item) => (
-          <div key={item} className="bg-zinc-200 rounded-xl gap-4 flex-col flex-1 items-center justify-start shadow ring-2 shadow-slate-900 flex p-4 w-full min-h-16">
+      <div className="grid mt-8 grid-cols-2 gap-4 items-center justify-center">
+        {vars.map((item) => (
+          <div key={item} className="bg-zinc-300 rounded-xl gap-4 flex-col flex-1 items-center justify-start shadow ring-2 shadow-slate-900 flex p-4 w-full min-h-16">
             <span className="text-center text-pretty text-xl">{item}</span>
             <textarea 
-              className="flex rounded-md w-17 h-8 justify-center text-center resize-none focus:ring-blue-400 focus:ring-3" 
-              onChange={(e) => {
-                e.target.value = e.target.value.replace(/[^0-9-.]/g, '')
-                getLens({cil: e.target.value, esf: e.target.value }).then((data) => {
-                  console.log(`data`, data)
-                  setData(data)
-                })
-              }}
-              placeholder={`${item}`} 
+              className="flex rounded-md w-16 h-8 justify-center text-center resize-none focus:ring-blue-400 focus:ring-3" 
+              id={item}
+              onChange={handleChange}
+              placeholder={`0.0`} 
             /> 
           </div>
         ))}
       </div>
       <div className="p-4 grid md:grid-cols-2 lg:grid-cols-4 gap-4 flex-1 items-center justify-center">
         {data?.length > 0 && 
-        data.slice(0, 8).map((elem:any,i) => (
+        data.slice(0, 8).map((elem:ModelCardProps,i) => (
           <ModelCard 
             key={"model" + i}
             brand={elem.brand} 
--- src/network/getLens.ts
+++ src/network/getLens.ts
@@ -3,6 +3,7 @@ export interface GetLensInput {
   cil?: string
   esf?: string
 }
+
 export async function getLens({ cil, esf } : GetLensInput) {
   const result = await fetch(`http://localhost:3005/api/lenses/lens?Y=${cil}&X=${esf}`)
   return result.json()
